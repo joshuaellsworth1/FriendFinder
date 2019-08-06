ec302645349db6324ca193798290ffc1137ddfa5
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-08-05 20:59:45 -0400

    survey html layout, added modal

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -14,8 +14,26 @@ module.exports = function (app) {
 
         var response = answer.score;
 
-        var Name = '';
+        var name = '';
         var image = '';
         var totalDifference = 100
 
-        
+        for (var i = 0; i < friends.length; i++) {
+            var diff = 0;
+            for (var j = 0; j < response.length; j++) {
+                diff += Math.abs(friends[i].score[j] - answer[j]);
+            }
+        }
+        if (diff < totalDifference) {
+            console.log('friend found = ' + diff)
+            console.log('Name of Friend =' + friends[i].name);
+            console.log('Friends image = ' + friends[i].image);
+
+            totalDifference = diff;
+            name = friends[i].name;
+            image = friends[i].photo;
+        }
+    });
+    friends.push(answer);
+    res.json({ name: name, image: image });
+}
