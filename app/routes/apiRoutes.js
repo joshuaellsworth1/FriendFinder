79aa0cd2e566821dc122bf23e551ff7a33e26ad2
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-08-06 16:00:02 -0400

    apiRoutes js finished. Friend displaying in modal

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -15,25 +15,43 @@ module.exports = function (app) {
         var name = "";
         var image = "";
         var match = 0;
-        var totalDifference = 0;
+        var bestDifference = 50;
 
+        console.log(user);
+
+        // Looks through our friends for comparison in the inner loop
         for (var i = 0; i < friends.length; i++) {
-            console.log(friends[i].name);
+            // console.log(friends[i].name);
+            // console.log(friends[i].scores);
+            var totalDifference = 0;
+
+            // Compares scores of friends[i] with user.scores
+            for (var j = 0; j < friends[i].scores.length; j++) {
+                //console.log(friends[i].scores[j]);
+                //console.log(user.scores[j]);
+                var scoreDifference = Math.abs(friends[i].scores[j] - parseInt(user.scores[j])); 
+                //console.log(difference)
+                totalDifference += scoreDifference;
+                // console.log("Friend: " + friends[i].name);
+                // console.log("Friend Score: " + friends[i].scores[j]);
+                // console.log("User Score: " + user.scores[j]);
+                // console.log("Score Difference " + j + ": " + scoreDifference);
+                // console.log("Total Difference: " + totalDifference);
+                // console.log("------------------------");
+            }
+
+            if (bestDifference > totalDifference) {
+                bestDifference = totalDifference; // Update the bestDifference with the new totalDifference if it is less
+                match = i; // Update the index of the new best match
+            }
 
         }
-        for (var j = 0; j < friends.scores.length; j++) {
-            console.log(friends.scores[j]);
-            console.log(user.scores[j]);
-            var difference = Math.abs(friends.length[j] - user.length[j]); {
-                if (totalDifference += difference)
-                    return (true);
-            }
-        }
-        // for (var i = 0; i < user.length; i++) {
-        //     console.log(user.length[i])
-        // }
+
+        console.log("------------------------");
+        console.log("Best Match: " + friends[match].name);
+        console.log("Best Difference: " + bestDifference);
 
         friends.push(user);
-        res.json({ name: name, image: image });
+        res.json(friends[match]);
     });
 }
