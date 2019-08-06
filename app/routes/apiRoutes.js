7d9cfc314dfe55e5597c62e5571b09489d0c8109
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-08-06 14:55:58 -0400

    continued working on apiRoutes.js

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -14,19 +14,25 @@ module.exports = function (app) {
         var response = user.score;
         var name = "";
         var image = "";
-
-        // function difference(a, b) {
-        //     return Math.abs(a - b);
-        //   }
+        var match = 0;
+        var totalDifference = 0;
 
         for (var i = 0; i < friends.length; i++) {
-            console.log(friends[i]);
+            console.log(friends[i].name);
 
-            for (var j = 0; j < response.length; j++) {
-                if (friends.length[0] == user.res)
-                    console.log("congrats you match!");
+        }
+        for (var j = 0; j < friends.scores.length; j++) {
+            console.log(friends.scores[j]);
+            console.log(user.scores[j]);
+            var difference = Math.abs(friends.length[j] - user.length[j]); {
+                if (totalDifference += difference)
+                    return (true);
             }
         }
+        // for (var i = 0; i < user.length; i++) {
+        //     console.log(user.length[i])
+        // }
+
         friends.push(user);
         res.json({ name: name, image: image });
     });
